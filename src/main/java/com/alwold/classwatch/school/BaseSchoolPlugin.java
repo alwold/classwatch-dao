b42063e9348319d7c7b868841f9a4efc31c3b0d3
Al Wold <alwold@gmail.com>
2012-01-20 11:02:04 -0700

    only do the serialize if trace logging is on

--- src/main/java/com/alwold/classwatch/school/BaseSchoolPlugin.java
+++ src/main/java/com/alwold/classwatch/school/BaseSchoolPlugin.java
@@ -3,6 +3,7 @@ package com.alwold.classwatch.school;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import org.apache.log4j.Logger;
 import org.apache.xml.serialize.OutputFormat;
 import org.apache.xml.serialize.XMLSerializer;
 import org.w3c.dom.Document;
@@ -12,17 +13,21 @@ import org.w3c.dom.Document;
  * @author alwold
  */
 public abstract class BaseSchoolPlugin implements SchoolPlugin {
+	private static Logger logger = Logger.getLogger(BaseSchoolPlugin.class);
+	
 	protected void serializeDoc(Document doc) {
-//		try {
-//			XMLSerializer xmls = new XMLSerializer(System.out, new OutputFormat());
-//			xmls.serialize(doc);
-//			File file = File.createTempFile("debug", ".html");
-//			xmls = new XMLSerializer(new FileWriter(file), new OutputFormat());
-//			xmls.serialize(doc);
-//			System.out.println("saved to file: "+file.getPath());
-//		} catch (IOException ex) {
-//			ex.printStackTrace();
-//		}
+		if (logger.isTraceEnabled()) {
+			try {
+				XMLSerializer xmls = new XMLSerializer(System.out, new OutputFormat());
+				xmls.serialize(doc);
+				File file = File.createTempFile("debug", ".html");
+				xmls = new XMLSerializer(new FileWriter(file), new OutputFormat());
+				xmls.serialize(doc);
+				System.out.println("saved to file: "+file.getPath());
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 	
 }
