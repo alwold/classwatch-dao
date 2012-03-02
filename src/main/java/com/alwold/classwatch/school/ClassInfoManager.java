ad6ee9b0b5e5771639b5f1f91ed9180cc08df4f1
Al Wold <alwold@gmail.com>
2012-03-02 16:19:22 -0700

    add method to check status of class

--- src/main/java/com/alwold/classwatch/school/ClassInfoManager.java
+++ src/main/java/com/alwold/classwatch/school/ClassInfoManager.java
@@ -1,6 +1,7 @@
 package com.alwold.classwatch.school;
 
 import com.alwold.classwatch.model.School;
+import com.alwold.classwatch.model.Status;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.orm.jpa.support.JpaDaoSupport;
 
@@ -11,10 +12,29 @@ import org.springframework.orm.jpa.support.JpaDaoSupport;
 public class ClassInfoManager extends JpaDaoSupport {
 	@Cacheable("classes")
 	public ClassInfo getClassInfo(Long institution, String termCode, String classNumber) throws RetrievalException {
+		SchoolPlugin plugin = getSchoolPlugin(institution);
+		if (plugin != null) {
+			return plugin.getClassInfo(termCode, classNumber);
+		} else {
+			return null;
+		}
+	}
+	
+	@Cacheable("classStatuses")
+	public Status getClassStatus(Long institution, String termCode, String classNumber) throws RetrievalException {
+		SchoolPlugin plugin = getSchoolPlugin(institution);
+		if (plugin != null) {
+			return plugin.getClassStatus(termCode, classNumber);
+		} else {
+			return null;
+		}
+	}
+	
+	private SchoolPlugin getSchoolPlugin(Long institution) {
 		School school = getJpaTemplate().find(School.class, institution);
 		try {
 			SchoolPlugin plugin = (SchoolPlugin) Class.forName(school.getPluginClass()).newInstance();
-			return plugin.getClassInfo(termCode, classNumber);
+			return plugin;
 		} catch (ClassNotFoundException e) {
 			logger.error("Unable to instantiate school plugin", e);
 			return null;
